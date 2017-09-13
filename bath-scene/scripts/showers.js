2a96735e42de94908f0b9f4ad19561f3ee5240f8
Daniel David Kovacs <v-dakovacs@expedia.com>
2017-09-13 14:27:09 +0200

    shower temp adder

--- bath-scene/scripts/showers.js
+++ bath-scene/scripts/showers.js
@@ -2,10 +2,31 @@
 - open popup 
 */
 (function(){
+    var showerTemps = [7, 23, 59, 61];
+    var showerFlows = [0,0,0,0];
+    const MAX_FLOW = 4;
+
+    function calcTemp() {
+        var flows = showerFlows.reduce((a, b) => a + b);
+        var weightedTemps = showerTemps
+            .map((value, index) => value * showerFlows[index])
+            .reduce((a, b) => a + b);
+        return flows > 0 ? weightedTemps / flows : undefined;
+    }
+
+    showerTemps.forEach((value, number) => {
+        $(`#temp_${number}`).text(value);
+    });
+
     $('#shower-popup-mask').on('click', event => {
         $(event.target).parent().hide();
     });
     $('#shower-small').on('click', event => {
         $('#shower-popup').show();
+    });
+    $('.shower-booth').on('click', event => {
+        var showerClicked = parseInt(event.target.id.split('_')[1]);
+        showerFlows[showerClicked] = (showerFlows[showerClicked] + 1) % MAX_FLOW;
+        console.log(`Temp: ${calcTemp()}`);
     });
 })();
