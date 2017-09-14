cb5e38209f013b137b5ef0e6f2c022c38de76ddf
Daniel David Kovacs <v-dakovacs@expedia.com>
2017-09-14 16:31:33 +0200

    wip

--- bath-scene/scripts/epilogue.js
+++ bath-scene/scripts/epilogue.js
@@ -1,3 +1,13 @@
 function endSequence() {
     $('#epilogue').show();
 }
+
+(function(){
+    $('#qr-mini').on('click', event => {
+        $(event.target.id).hide();
+        $('#qr').show();
+    });
+    $('#qr').on('click', event => {
+
+    });
+})();
